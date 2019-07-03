16fb93c67d220eb69c729f7dd8b5a27be3413760
manumunoz <manumunozh@gmail.com>
2019-07-03 14:27:30 +0400

    Final Pay ES

---
+++ pay_es/pages.py
@@ -0,0 +1,19 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class RandomPayWP(WaitPage):
+    def after_all_players_arrive(self):
+        self.group.round_payoffs()
+        self.group.payoff_value()
+
+
+class RandomPay_es(Page):
+    pass
+
+
+page_sequence = [
+    RandomPayWP,
+    RandomPay_es,
+]
